3ee9e2f4451de476cc55e5fa6f96cedc8ae85037
getsuki <37655180+getsuki@users.noreply.github.com>
2023-09-22 01:46:06 +0000

    logback update 22/9

--- simple-player-stats-jpa/src/main/java/sg/ntu/edu/simpleplayerstats/SimplePlayerStatsApplication.java
+++ simple-player-stats-jpa/src/main/java/sg/ntu/edu/simpleplayerstats/SimplePlayerStatsApplication.java
@@ -1,42 +1,37 @@
 package sg.ntu.edu.simpleplayerstats;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import sg.ntu.edu.simpleplayerstats.controller.PlayerController;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @SpringBootApplication
 public class SimplePlayerStatsApplication {
 
-	private static final Logger logger = LoggerFactory.getLogger(SimplePlayerStatsApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(SimplePlayerStatsApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimplePlayerStatsApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer configure() {
-		return new WebMvcConfigurer() {
-
-			@Override
-			public void addCorsMappings(CorsRegistry corsRegistry) {
-				corsRegistry.addMapping("/**").allowedOrigins("*");
-			}
-
-		};
-	}
-	
     public static void main(String[] args) {
-        logger.info("🟢 Starting Soccer Stats API");
-        logger.debug("❓ An exception occurred");
+        logger.info("🟢 Starting SimplePlayerStatsApplication");
+        logger.debug("❓ Debugging information...An exception occurred");
         logger.warn("🟠 Test Warning");
         logger.error("🔴 Error Warning");
-        SpringApplication.run(PlayerController.class, args);
+        SpringApplication.run(SimplePlayerStatsApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**").allowedOrigins("*");
+            }
+
+        };
+    }
+
 }
+
